c3fd83c6e2fdaa2447a3eaa3552ebc9ae2c9e7b2
Tsebo <tsbletele@gmail.com>
2026-02-05 13:52:14 +0200

    refactor: added Api controller

--- Apple Music Dashboard/Controllers/AppleMusicController.cs
+++ Apple Music Dashboard/Controllers/AppleMusicController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Apple_Music_Dashboard.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Apple_Music_Dashboard.Controllers
 {
-    public class AppleMusicController : Controller
+    [ApiController]
+    [Route("api/apple-music")]
+    public class AppleMusicController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly AppleMusicApiService _api;
+
+        public AppleMusicController(AppleMusicApiService api)
         {
-            return View();
+            _api = api;
+        }
+
+        [HttpGet("library/songs")]
+        public async Task<IActionResult> GetSongs(
+            [FromHeader(Name = "Music-User-Token")] string userToken)
+        {
+            var devToken = /* generate or fetch */;
+            var result = await _api.GetLibrarySongsAsync(devToken, userToken);
+            return Ok(result);
         }
     }
+
 }
