535597416d6af91630aaaa86d2c0e4b7f54f839b
Tsebo <tsbletele@gmail.com>
2026-02-05 13:51:05 +0200

    refactor: added Stat service

--- Apple Music Dashboard/Services/StatsService.cs
+++ Apple Music Dashboard/Services/StatsService.cs
@@ -2,5 +2,18 @@
 {
     public class StatsService
     {
+        public int CountSongs(LibrarySongsResponse response)
+            => response.Data.Count;
+
+        public Dictionary<string, int> GenreDistribution(
+            IEnumerable<LibrarySong> songs)
+        {
+            return songs
+                .Where(s => s.Attributes?.GenreNames != null)
+                .SelectMany(s => s.Attributes.GenreNames)
+                .GroupBy(g => g)
+                .ToDictionary(g => g.Key, g => g.Count());
+        }
     }
+
 }
