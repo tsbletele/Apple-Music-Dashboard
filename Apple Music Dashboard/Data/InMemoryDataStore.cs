a6a319aa6e70d66ecaecb21502365ed006807e1a
Tsebo <tsbletele@gmail.com>
2026-02-06 19:14:33 +0200

    docs: added temporary data storage class

---
+++ Apple Music Dashboard/Data/InMemoryDataStore.cs
@@ -0,0 +1,14 @@
+﻿using Apple_Music_Dashboard.Models.Enitities;
+
+namespace Apple_Music_Dashboard.Data
+{
+    public static class InMemoryDataStore
+    {
+        public static User User = new();
+
+        public static List<Artist> Artists = new();
+        public static List<Song> Songs = new();
+        public static List<PlayEvent> PlayEvents = new();
+    }
+
+}
