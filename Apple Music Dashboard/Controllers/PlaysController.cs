a130a2b13ca8f7953f3d8dd4d0a5412a4a98e3d9
Tsebo <tsbletele@gmail.com>
2026-02-07 23:51:59 +0200

    Refactor play tracking: replace ListeningController
    Removed ListeningController and added PlaysController with RESTful
    endpoints for tracking and retrieving play events. PlaysController
    accepts PlayEvent objects, improving flexibility and structure.

---
+++ Apple Music Dashboard/Controllers/PlaysController.cs
@@ -0,0 +1,31 @@
+﻿using Apple_Music_Dashboard.Models;
+using Apple_Music_Dashboard.Models.Enitities;
+using Apple_Music_Dashboard.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apple_Music_Dashboard.Controllers;
+
+[ApiController]
+[Route("api/plays")]
+public class PlaysController : ControllerBase
+{
+    private readonly PlayTrackingService _service;
+
+    public PlaysController(PlayTrackingService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public IActionResult TrackPlay([FromBody] PlayEvent play)
+    {
+        _service.TrackPlay(play);
+        return Ok(play);
+    }
+
+    [HttpGet]
+    public IActionResult GetPlays()
+    {
+        return Ok(_service.GetAllPlays());
+    }
+}
