2798b02b36824dc82a1c48f116f4c8daa01c1c94
Tsebo <tsbletele@gmail.com>
2026-02-10 13:25:12 +0200

    Add WrappedController with weekly wrapped API endpoint
    Introduced WrappedController to provide an API endpoint at /api/wrapped/weekly. This controller uses WrappedService to return weekly wrapped data for the Apple Music dashboard.

---
+++ Apple Music Dashboard/Controllers/WrappedController.cs
@@ -0,0 +1,22 @@
+﻿using Apple_Music_Dashboard.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apple_Music_Dashboard.Controllers;
+
+[ApiController]
+[Route("api/wrapped")]
+public class WrappedController : ControllerBase
+{
+    private readonly WrappedService _wrappedService;
+
+    public WrappedController(WrappedService wrappedService)
+    {
+        _wrappedService = wrappedService;
+    }
+
+    [HttpGet("weekly")]
+    public IActionResult GetWeeklyWrapped()
+    {
+        return Ok(_wrappedService.GetWeeklyWrapped());
+    }
+}
