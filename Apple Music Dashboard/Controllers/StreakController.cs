915908fdba246cb203e8627067122cc6391dbdc6
Tsebo <tsbletele@gmail.com>
2026-02-11 14:34:56 +0200

    Add StreakController with GET endpoint for streak stats
    Introduced StreakController API at route "api/streaks" with a GET endpoint that returns streak statistics using StreakService. This enables clients to retrieve streak stats via a RESTful API.

---
+++ Apple Music Dashboard/Controllers/StreakController.cs
@@ -0,0 +1,22 @@
+﻿using Apple_Music_Dashboard.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apple_Music_Dashboard.Controllers;
+
+[ApiController]
+[Route("api/streaks")]
+public class StreakController : ControllerBase
+{
+    private readonly StreakService _streakService;
+
+    public StreakController(StreakService streakService)
+    {
+        _streakService = streakService;
+    }
+
+    [HttpGet]
+    public IActionResult GetStreakStats()
+    {
+        return Ok(_streakService.GetStreakStats());
+    }
+}
