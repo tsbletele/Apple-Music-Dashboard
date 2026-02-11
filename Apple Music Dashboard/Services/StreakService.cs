55324c803e4aabe5ead8fe64a0a56789d5280eaf
Tsebo <tsbletele@gmail.com>
2026-02-11 14:32:02 +0200

    Add StreakService for calculating listening streak stats
    Introduced StreakService with a GetStreakStats method to compute current streak, longest streak, and last listened date from play event data in the in-memory store. Returns results as a StreakStats object.

---
+++ Apple Music Dashboard/Services/StreakService.cs
@@ -0,0 +1,44 @@
+﻿using Apple_Music_Dashboard.Data;
+using Apple_Music_Dashboard.Models;
+
+namespace Apple_Music_Dashboard.Services;
+
+public class StreakService
+{
+    public StreakStats GetStreakStats()
+    {
+        var playDates = InMemoryDataStore.PlayEvents
+            .Select(p => p.PlayedAt.Date)
+            .Distinct()
+            .OrderByDescending(d => d)
+            .ToList();
+
+        if (!playDates.Any())
+        {
+            return new StreakStats();
+        }
+
+        int currentStreak = 1;
+        int longestStreak = 1;
+
+        for (int i = 1; i < playDates.Count; i++)
+        {
+            if ((playDates[i - 1] - playDates[i]).Days == 1)
+            {
+                currentStreak++;
+                longestStreak = Math.Max(longestStreak, currentStreak);
+            }
+            else
+            {
+                currentStreak = 1;
+            }
+        }
+
+        return new StreakStats
+        {
+            CurrentStreak = currentStreak,
+            LongestStreak = longestStreak,
+            LastListenedDate = playDates.First()
+        };
+    }
+}
